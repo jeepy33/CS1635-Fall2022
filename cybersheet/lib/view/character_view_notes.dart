7aef420b730072163c0ddf003d0ecf1f4b65fd90
alexaspaventa <alexaspaventa@gmail.com>
2022-12-10 18:21:25 -0500

    Notes stuff

--- cybersheet/lib/view/character_view_notes.dart
+++ cybersheet/lib/view/character_view_notes.dart
@@ -14,7 +14,7 @@ class _CharacterNotesState extends State<CharacterNotes> {
 
   List<String> crisisList = FamilyCrisis;
   String crisis = FamilyCrisis[0];
-
+  String saveNotes = "";
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -23,7 +23,39 @@ class _CharacterNotesState extends State<CharacterNotes> {
         image: AssetImage("Cyberwire.png"),
         fit: BoxFit.cover,
       )),
-      child: Center(),
+      child: Center(
+        child: Column(children: [
+          const SizedBox(
+            height: 15,
+          ),
+          const Text('Notes:',
+              style: TextStyle(fontSize: 25, color: Colors.white)),
+          const SizedBox(height: 15),
+          TextField(
+            onChanged: (text) {
+              print(text);
+              saveNotes = text;
+            },
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(
+            height: 60,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: buttonRed,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              textStyle: const TextStyle(fontSize: 20, fontFamily: 'Kontakt'),
+            ),
+            onPressed: () {
+              setState(() {});
+            },
+            child: const Text('Save Notes'),
+          ),
+        ]),
+      ),
     );
   }
 }

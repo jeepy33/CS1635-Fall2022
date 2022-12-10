c4ac39f9528762cd05c1ec1bea4b08ba0bef8932
alexaspaventa <alexaspaventa@gmail.com>
2022-12-09 21:27:19 -0500

    Styling background and appbar

--- cybersheet/lib/main.dart
+++ cybersheet/lib/main.dart
@@ -30,55 +30,68 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(228, 228, 35, 24),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          const Text(
-            'Home',
-            style: TextStyle(fontSize: 35, color: Colors.white),
-          ),
-          const SizedBox(height: 30),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 231, 106, 106),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 60, vertical: 25),
-                  textStyle:
-                      const TextStyle(fontSize: 20, fontFamily: 'Kontakt'),
-                ),
-                onPressed: () {
-                  Navigator.pushNamed(context, newCharRoute,
-                      arguments: 'arguments/chose NEW');
-                },
-                child: const Text('New Edgerunner'),
-              ),
-              const SizedBox(height: 20, width: 30),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromARGB(255, 231, 106, 106),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 60, vertical: 25),
-                  textStyle:
-                      const TextStyle(fontSize: 20, fontFamily: 'Kontakt'),
-                ),
-                onPressed: () {
-                  Navigator.pushNamed(context, editCharRoute,
-                      arguments: 'arguments/chose EDIT');
-                },
-                child: const Text('Edit Edgerunner'),
-              )
-            ],
-          ),
-        ],
-      )),
-    );
+        backgroundColor: Color.fromARGB(228, 228, 35, 24),
+        body: Center(
+          child: Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                image: AssetImage("Cyberwire.png"),
+                fit: BoxFit.cover,
+              )),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                      padding: EdgeInsets.only(bottom: 5),
+                      child: Image(
+                          image: AssetImage("Cybersheet_Logo.png"),
+                          width: 1000)),
+                  Padding(
+                      padding: EdgeInsets.only(bottom: 0),
+                      child: const Text(
+                        'Home',
+                        style: TextStyle(fontSize: 35, color: Colors.white),
+                      )),
+                  const SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(255, 231, 106, 106),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 60, vertical: 25),
+                          textStyle: const TextStyle(
+                              fontSize: 20, fontFamily: 'Kontakt'),
+                        ),
+                        onPressed: () {
+                          Navigator.pushNamed(context, newCharRoute,
+                              arguments: 'arguments/chose NEW');
+                        },
+                        child: const Text('New Edgerunner'),
+                      ),
+                      const SizedBox(height: 20, width: 30),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(255, 231, 106, 106),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 60, vertical: 25),
+                          textStyle: const TextStyle(
+                              fontSize: 20, fontFamily: 'Kontakt'),
+                        ),
+                        onPressed: () {
+                          Navigator.pushNamed(context, editCharRoute,
+                              arguments: 'arguments/chose EDIT');
+                        },
+                        child: const Text('Edit Edgerunner'),
+                      )
+                    ],
+                  ),
+                ],
+              )),
+        ));
   }
 }
 
@@ -98,128 +111,152 @@ class _newCharState extends State<newChar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(228, 228, 35, 24),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text('Template Selection: $template',
-                style: TextStyle(fontSize: 25, color: Colors.white)),
-            const SizedBox(height: 30),
-            ToggleButtons(
-              renderBorder: false,
-              isSelected: _selections,
-              onPressed: (int index) {
-                setState(() {
-                  _selections[index] = !_selections[index];
-                });
-              },
+        appBar: AppBar(
+          toolbarHeight: 120,
+          backgroundColor: Color.fromARGB(228, 228, 35, 24),
+          automaticallyImplyLeading: false,
+          title: Image(
+            image: AssetImage("Cybersheet_Logo.png"),
+            width: 400,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Color.fromARGB(228, 228, 35, 24),
+        body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage("Cyberwire.png"),
+            fit: BoxFit.cover,
+          )),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                TextButton(
-                    style: TextButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 60, vertical: 25),
-                        foregroundColor: Colors.white,
-                        backgroundColor: _selections[0]
-                            ? Colors.grey
-                            : Color.fromARGB(255, 231, 106, 106)),
-                    onPressed: () {
+                Text('Template Selection: $template',
+                    style: TextStyle(fontSize: 30, color: Colors.white)),
+                const SizedBox(height: 30),
+                ToggleButtons(
+                  renderBorder: false,
+                  isSelected: _selections,
+                  onPressed: (int index) {
+                    setState(() {
+                      _selections[index] = !_selections[index];
+                    });
+                  },
+                  children: <Widget>[
+                    TextButton(
+                        style: TextButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 60, vertical: 25),
+                            foregroundColor: _selections[0]
+                                ? Color.fromARGB(255, 231, 106, 106)
+                                : Colors.white,
+                            backgroundColor: _selections[0]
+                                ? Colors.white
+                                : Color.fromARGB(255, 231, 106, 106)),
+                        onPressed: () {
+                          setState(() {
+                            presenter.model.character
+                                .setTemplate(Template.STREETRAT);
+                            template = Template.STREETRAT.toString();
+                            _selections.fillRange(0, _selections.length, false);
+                            _selections[0] = true;
+                          });
+                        },
+                        child: const Text('Street Rat',
+                            style: TextStyle(fontSize: 20))),
+                    TextButton(
+                        style: TextButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 60, vertical: 25),
+                            foregroundColor: _selections[1]
+                                ? Color.fromARGB(255, 231, 106, 106)
+                                : Colors.white,
+                            backgroundColor: _selections[1]
+                                ? Colors.white
+                                : Color.fromARGB(255, 231, 106, 106)),
+                        onPressed: () {
+                          setState(() {
+                            presenter.model.character
+                                .setTemplate(Template.EDGERUNNER);
+                            template = Template.EDGERUNNER.toString();
+                            _selections.fillRange(0, _selections.length, false);
+                            _selections[1] = true;
+                          });
+                        },
+                        child: const Text('Edgerunner',
+                            style: TextStyle(fontSize: 20))),
+                    TextButton(
+                        style: TextButton.styleFrom(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 40, vertical: 25),
+                            foregroundColor: _selections[2]
+                                ? Color.fromARGB(255, 231, 106, 106)
+                                : Colors.white,
+                            backgroundColor: _selections[2]
+                                ? Colors.white
+                                : Color.fromARGB(255, 231, 106, 106)),
+                        onPressed: () {
+                          setState(() {
+                            presenter.model.character
+                                .setTemplate(Template.COMPLETE);
+                            template = Template.COMPLETE.toString();
+                            _selections.fillRange(0, _selections.length, false);
+                            _selections[2] = true;
+                          });
+                        },
+                        child: const Text('Complete Package',
+                            style: TextStyle(fontSize: 20)))
+                  ],
+                ),
+                const SizedBox(
+                  height: 60,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 231, 106, 106),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 15),
+                    textStyle:
+                        const TextStyle(fontSize: 20, fontFamily: 'Kontakt'),
+                  ),
+                  onPressed: () {
+                    if (template.isEmpty) {
                       setState(() {
-                        presenter.model.character
-                            .setTemplate(Template.STREETRAT);
-                        template = Template.STREETRAT.toString();
+                        int r = presenter.rand.nextInt(3);
+                        Template temp = Template.values[r];
+                        presenter.model.character.setTemplate(temp);
+                        template = temp.toString();
                         _selections.fillRange(0, _selections.length, false);
-                        _selections[0] = true;
+                        _selections[r] = true;
                       });
-                    },
-                    child: const Text('Street Rat',
-                        style: TextStyle(fontSize: 20))),
-                TextButton(
-                    style: TextButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 60, vertical: 25),
-                        foregroundColor: Colors.white,
-                        backgroundColor: _selections[1]
-                            ? Colors.grey
-                            : Color.fromARGB(255, 231, 106, 106)),
-                    onPressed: () {
-                      setState(() {
-                        presenter.model.character
-                            .setTemplate(Template.EDGERUNNER);
-                        template = Template.EDGERUNNER.toString();
-                        _selections.fillRange(0, _selections.length, false);
-                        _selections[1] = true;
-                      });
-                    },
-                    child: const Text('Edgerunner',
-                        style: TextStyle(fontSize: 20))),
-                TextButton(
-                    style: TextButton.styleFrom(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 40, vertical: 25),
-                        foregroundColor: Colors.white,
-                        backgroundColor: _selections[2]
-                            ? Colors.grey
-                            : Color.fromARGB(255, 231, 106, 106)),
-                    onPressed: () {
-                      setState(() {
-                        presenter.model.character
-                            .setTemplate(Template.COMPLETE);
-                        template = Template.COMPLETE.toString();
-                        _selections.fillRange(0, _selections.length, false);
-                        _selections[2] = true;
-                      });
-                    },
-                    child: const Text('Complete Package',
-                        style: TextStyle(fontSize: 20)))
+                    }
+                    Navigator.pushNamed(context, templatesRoute,
+                        arguments: 'arguments/chose Templates');
+                  },
+                  child: const Text('Next'),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 231, 106, 106),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 15),
+                    textStyle:
+                        const TextStyle(fontSize: 20, fontFamily: 'Kontakt'),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Back'),
+                )
               ],
             ),
-            const SizedBox(
-              height: 60,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 231, 106, 106),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                textStyle: const TextStyle(fontSize: 20, fontFamily: 'Kontakt'),
-              ),
-              onPressed: () {
-                if (template.isEmpty) {
-                  setState(() {
-                    int r = presenter.rand.nextInt(3);
-                    Template temp = Template.values[r];
-                    presenter.model.character.setTemplate(temp);
-                    template = temp.toString();
-                    _selections.fillRange(0, _selections.length, false);
-                    _selections[r] = true;
-                  });
-                }
-                Navigator.pushNamed(context, templatesRoute,
-                    arguments: 'arguments/chose Templates');
-              },
-              child: const Text('Next'),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 231, 106, 106),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                textStyle: const TextStyle(fontSize: 20, fontFamily: 'Kontakt'),
-              ),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('Back'),
-            )
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
 

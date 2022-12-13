fe5d4ff06378d42e54a362cf9f20a47355970e33
halanubani <60750262+halanubani@users.noreply.github.com>
2022-12-13 14:27:15 -0500

    mainly cyberware and skills

--- cybersheet/lib/view/character_view_cyberware.dart
+++ cybersheet/lib/view/character_view_cyberware.dart
@@ -23,7 +23,110 @@ class _CharacterCyberwareState extends State<CharacterCyberware> {
         image: AssetImage("Cyberwire.png"),
         fit: BoxFit.cover,
       )),
-      child: Center(),
+      child: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            child: Image(
+              image: AssetImage("Body3.png"),
+              width: 300,
+              height: 250,
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0,
+            child: Image(
+              image: AssetImage("BodyNoShade.png"),
+              width: 300,
+              height: 250,
+            ),
+          ),
+          Positioned(
+            left: 820,
+            bottom: 700,
+            child: FloatingActionButton(
+              mini: true,
+              backgroundColor: Color.fromRGBO(255, 255, 255, 0),
+              foregroundColor: Colors.white,
+              onPressed: () {
+                // Handle the button press
+              },
+              child: Icon(Icons.add_circle_outline_rounded),
+            ),
+          ),
+          Positioned(
+            left: 750,
+            bottom: 600,
+            child: FloatingActionButton(
+              mini: true,
+              backgroundColor: Color.fromRGBO(255, 255, 255, 0),
+              foregroundColor: Colors.white,
+              onPressed: () {
+                // Handle the button press
+              },
+              child: Icon(Icons.add_circle_outline_rounded),
+            ),
+          ),
+          Positioned(
+            left: 1000,
+            bottom: 550,
+            child: FloatingActionButton(
+              mini: true,
+              backgroundColor: Color.fromRGBO(255, 255, 255, 0),
+              foregroundColor: Colors.white,
+              onPressed: () {
+                // Handle the button press
+              },
+              child: Icon(Icons.add_circle_outline_rounded),
+            ),
+          ),
+          Positioned(
+            left: 750,
+            bottom: 400,
+            child: FloatingActionButton(
+              mini: true,
+              backgroundColor: Color.fromRGBO(255, 255, 255, 0),
+              foregroundColor: Colors.white,
+              onPressed: () {
+                // Handle the button press
+              },
+              child: Icon(Icons.add_circle_outline_rounded),
+            ),
+          ),
+          Positioned(
+            left: 750,
+            bottom: 200,
+            child: FloatingActionButton(
+              mini: true,
+              backgroundColor: Color.fromRGBO(255, 255, 255, 0),
+              foregroundColor: Colors.white,
+              onPressed: () {
+                // Handle the button press
+              },
+              child: Icon(Icons.add_circle_outline_rounded),
+            ),
+          ),
+          Positioned(
+            left: 950,
+            bottom: 200,
+            child: FloatingActionButton(
+              mini: true,
+              backgroundColor: Color.fromRGBO(255, 255, 255, 0),
+              foregroundColor: Colors.white,
+              onPressed: () {
+                // Handle the button press
+              },
+              child: Icon(Icons.add_circle_outline_rounded),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- cybersheet/lib/view/character_view_skills.dart
+++ cybersheet/lib/view/character_view_skills.dart
@@ -3,6 +3,8 @@
 import 'package:flutter/material.dart';
 import '../constants.dart';
 import 'package:tabbed_view/tabbed_view.dart';
+import '../main.dart';
+import '../model/model.dart';
 
 class CharacterSkills extends StatefulWidget {
   const CharacterSkills({super.key});
@@ -16,8 +18,32 @@ class CharacterSkills extends StatefulWidget {
 class _CharacterSkillsState extends State<CharacterSkills> {
   List<String> environmentList = ChildhoodEnvironment;
   String environment = ChildhoodEnvironment[0];
+  Map<String, String> itemMap = awarenessSkills;
   late TabbedViewController _controller;
+  final key = 'someKey';
+  final controller = TextEditingController();
+  List<Widget> widgets = [];
+
+// Loop through the keys in the map
+// for (String key in rangedWeaponSkills.keys) {
+//   // Create a TextField for each key in the map
+//   widgets.add(
+//     TextField(
+//       // Bind the value of the TextField to the corresponding value in the map
+//       controller: TextEditingController(text: rangedWeaponSkills[key]),
+//       textAlign: TextAlign.left,
+//       onChanged: (value) {
+//         // Update the value of the item in the map when the user changes the value in the TextField
+//         setState(() {
+//           rangedWeaponSkills[key] = value;
+//         });
+//       },
+//     ),
+//   );
+// }
+
   List<String> crisisList = FamilyCrisis;
+
   String crisis = FamilyCrisis[0];
 
   @override
@@ -27,19 +53,19 @@ class _CharacterSkillsState extends State<CharacterSkills> {
 
     tabs.add(TabData(
         text: 'Awareness',
-        content: Padding(child: Text('Hello'), padding: EdgeInsets.all(8))));
-    tabs.add(TabData(
-        text: 'Body',
-        content:
-            //   print(awarenessSkills);
-            // awarenessSkills.map((a,b) => print(a)).toList();));
-            //Text(awarenessSkills),
-            // awarenessSkills.forEach((k,v) => print('${k}: ${v}'));
-            const Padding(padding: EdgeInsets.all(8))));
+        content: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Text(mapToString(awarenessSkills)))));
     tabs.add(TabData(
         closable: false,
-        text: 'Control',
+        // keepAlive: true,
+        text: 'Body',
         content: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Text(mapToString(bodySkills)))));
+    tabs.add(TabData(
+        text: 'Control',
+        content: const Padding(
             child: TextField(
                 decoration: InputDecoration(
                     isDense: true, border: OutlineInputBorder())),
@@ -47,24 +73,54 @@ class _CharacterSkillsState extends State<CharacterSkills> {
         keepAlive: true));
     tabs.add(TabData(
         text: 'Education',
-        content:
-            Padding(child: Text('Hello again'), padding: EdgeInsets.all(8))));
+        content: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Text(mapToString(educationSkills)))));
     tabs.add(TabData(
         text: 'Fighting',
-        content:
-            Padding(child: Text('Hello again'), padding: EdgeInsets.all(8))));
+        content: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Text(mapToString(fightingSkills)))));
     tabs.add(TabData(
         text: 'Preformance',
-        content:
-            Padding(child: Text('Hello again'), padding: EdgeInsets.all(8))));
-    tabs.add(TabData(
+        content: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Text(mapToString(performanceSkills)))));
+
+    Set<Widget> widgets = {
+      for (String key in rangedWeaponSkills.keys)
+        TextField(
+          controller: TextEditingController(text: rangedWeaponSkills[key]),
+          textAlign: TextAlign.left,
+          onChanged: (value) {
+            setState(() {
+              rangedWeaponSkills[key] = value;
+            });
+          },
+        ),
+    };
+
+    tabs.add(
+      TabData(
         text: 'Ranged Weapon',
-        content:
-            Padding(child: Text('Hello again'), padding: EdgeInsets.all(8))));
+        content: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(children: <Widget>[
+            ...widgets,
+            Text(
+              mapToString(rangedWeaponSkills),
+              textAlign: TextAlign.left,
+            ),
+          ]),
+        ),
+      ),
+    );
+
     tabs.add(TabData(
         text: 'Technique',
-        content:
-            Padding(child: Text('Hello again'), padding: EdgeInsets.all(8))));
+        content: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Text(mapToString(techniqueSkills)))));
 
     _controller = TabbedViewController(tabs);
   }
@@ -83,5 +139,30 @@ class _CharacterSkillsState extends State<CharacterSkills> {
       child: w,
       padding: EdgeInsets.all(32),
     );
+  }
+
+  String mapToString(Map<String, String> map) {
+    String dex = presenter.model.character.stats.DEX.toString();
+    String hum = presenter.model.character.stats.HUM.toString();
+    String cool = presenter.model.character.stats.COOL.toString();
+    String will = presenter.model.character.stats.WILL.toString();
+    String tech = presenter.model.character.stats.DEX.toString();
+    String luck = presenter.model.character.stats.LUCK.toString();
+    String emp = presenter.model.character.stats.EMP.toString();
+    String ref = presenter.model.character.stats.REF.toString();
+    String int = presenter.model.character.stats.INT.toString();
+    String result = "";
+    map.forEach((key, value) {
+      if (map[key] == 'DEX') result += "$key: $dex\n";
+      if (map[key] == 'HUM') result += "$key: $hum\n";
+      if (map[key] == 'COOL') result += "$key: $cool\n";
+      if (map[key] == 'EMP') result += "$key: $emp\n";
+      if (map[key] == 'LUCK') result += "$key: $luck\n";
+      if (map[key] == 'WILL') result += "$key: $will\n";
+      if (map[key] == 'TECH') result += "$key: $tech\n";
+      if (map[key] == 'REF') result += "$key: $ref\n";
+      if (map[key] == 'INT') result += "$key: $int\n";
+    });
+    return result;
   }
 }
